32316c457ffef408796607bc943b862109e0bb57
Carlos Rojas <carlos.rojas@localhost>
2020-04-26 10:57:30 -0500

    adding firebase

---
+++ src/firebase-config.tsx
@@ -0,0 +1,8 @@
+export const firebaseConfig: any = {
+    apiKey: 'XXX',
+    authDomain: 'XXX',
+    databaseURL: 'XXX',
+    projectId: 'XXX',
+    storageBucket: 'XXX',
+    messagingSenderId: 'XXX'
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,71 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import { 
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItemSliding,
+  IonItem,
+  IonLabel,
+  IonText,
+  IonItemOptions,
+  IonItemOption
+} from '@ionic/react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
+import { db } from '../firebase';
 
 const Home: React.FC = () => {
+  const [itemsArray, setItemsArray] : any = useState([]);
+  const itemsRef = db.ref('items');
+  useEffect(() => {
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          text: items[item].text
+        });
+      }
+      setItemsArray(newState);
+    });
+  }, [itemsRef]);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>ionic-react database firebase</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">ionic-react database firebase</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonList>
+        {itemsArray.map( (item: any) => { 
+          return ( 
+          <IonItemSliding key={item.id}>
+            <IonItem>
+              <IonLabel>
+              <IonText color="primary">
+                <h3>{item.text}</h3>
+              </IonText>
+              <p>{item.description}</p>
+              </IonLabel>
+            </IonItem>
+            <IonItemOptions side="end">
+            <IonItemOption onClick={() => {}}>Edit</IonItemOption>
+            <IonItemOption onClick={() => {}} color="secondary">Delete</IonItemOption>     
+            </IonItemOptions>
+          </IonItemSliding>
+          ); 
+        })}
+        </IonList>
       </IonContent>
     </IonPage>
   );
